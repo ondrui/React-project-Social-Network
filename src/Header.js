850f73d8916db751c490bb222c6a08bf1e4adef3
ondrui <ondrui@yandex.ru>
2021-08-02 17:44:07 +0300

    add project step 1

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <a href="#w">Home</a>
+      <br />
+      <a href="#w">New Feed</a>
+      <br />
+      <a href="#w">Messages</a>
+    </div>
+  );
+};
+
+export default Header;
